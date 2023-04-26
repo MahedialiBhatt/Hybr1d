afb629ba3bfbfa44be8df701e1cd4bfa660c62f4
Mahediali <bhattmahediali92@gmail.com>
2023-04-27 00:22:36 +0530

    Added generic function for response and invoker

--- src/utils/utility.js
+++ src/utils/utility.js
@@ -14,8 +14,36 @@ async function generateHashPassword(password) {
   return hash;
 }
 
+function writeResponse(err, data, res) {
+  if (err) {
+    res.status(err.code && Number.isInteger(err.code) ? err.code : 500);
+    return res.json({
+      status: "error",
+      message: err.message,
+    });
+  }
+  res.status(200);
+  const response = {
+    status: "Success",
+    data: data,
+  };
+  return res.json(response);
+}
+
+function invoker(promise) {
+  return promise
+    .then((data) => {
+      return [data, null];
+    })
+    .catch((err) => {
+      return [null, err];
+    });
+}
+
 module.exports = {
   getLoggedInUserId,
   validatePasswordHash,
   generateHashPassword,
+  writeResponse,
+  invoker,
 };
